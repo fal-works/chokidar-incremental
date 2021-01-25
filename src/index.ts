33f4d8fad8e5bdd0eac142757a9aa27d14dcd8cb
FAL <falworks.contact@gmail.com>
2021-01-25 18:20:35 +0900

    feat: return onExit from onStart

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,42 @@ import { measureAndLog } from "./util/date.js";
 import type { WatchOptions, FSWatcher } from "chokidar";
 
 /**
+ * Functions to be called during the watch mode.
+ */
+export type WatchCallbacks<T> = {
+  onChange: (changedPath: string) => Promise<T>;
+  onExit?: NodeJS.ExitListener;
+};
+
+/**
  * Constructs and runs a file watcher.
  *
  * @param filePatterns Glob pattern(s) of files to watch.
- * @param onStart Called once when the watcher is ready and returns a callback
- * function to be run each time any file is changed.
+ * @param onStart Called once when the watcher is ready. This should return
+ * `onChange` and (optionally) `onExit`.
  */
 export const watch = async <T>(
   filePatterns: string | readonly string[],
-  onStart: () => Promise<(pattern: string) => Promise<T>>,
+  onStart: () => Promise<WatchCallbacks<T>>,
   options?: {
-    onExit?: NodeJS.ExitListener;
     onStartMessage?: () => string;
     onChangeMessage?: (result: T) => string;
     chokidarOptions?: WatchOptions;
   }
 ): Promise<FSWatcher> => {
-  const { onExit, onStartMessage, onChangeMessage, chokidarOptions } =
-    options || {};
+  const { onStartMessage, onChangeMessage, chokidarOptions } = options || {};
 
   const getStartMsg = onStartMessage || (() => "Completed onStart task.");
   const getChangeMsg = onChangeMessage || (() => "Completed onChange task.");
 
-  const onChangeCallback = await measureAndLog(onStart, undefined, getStartMsg);
-  const onChange = (path: string) =>
-    measureAndLog(onChangeCallback, path, getChangeMsg);
+  const watchCallbacks = await measureAndLog(onStart, undefined, getStartMsg);
+  const onChangeCallback = watchCallbacks.onChange;
 
   return createWatcher({
     paths: filePatterns,
-    onChange,
-    onExit,
+    onChange: (path: string) =>
+      measureAndLog(onChangeCallback, path, getChangeMsg),
+    onExit: watchCallbacks.onExit,
     chokidarOptions,
   });
 };
